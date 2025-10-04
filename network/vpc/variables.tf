31ded677159cc8feb67427aa2c8366ecddac70dc
vinay <vinayrhcsacertified@gmail.com>
2025-10-04 15:18:24 +0530

    test

--- network/vpc/variables.tf
+++ network/vpc/variables.tf
@@ -28,13 +28,11 @@ variable "tags" {
 variable "name" {
   description = "Name to be used on all the resources as identifier"
   type        = string
-  default     = ""
 }
 
 variable "cidr" {
   description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "secondary_cidr_blocks" {
